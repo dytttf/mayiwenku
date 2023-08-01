9b4da8852e66c473dd58cee540deb4d65350313d
dytttf <dytttf@foxmail.com>
2023-08-01 21:18:54 +0800

    尝试提升清晰度,然而并没有用,清晰度在图片转换成pdf的时候丢失了

--- mayiwenku.py
+++ mayiwenku.py
@@ -20,14 +20,14 @@ class MaYiWenKu(object):
         self.data_dir = data_dir or cur_path
         #
         self.base_headers = {
-            'Accept': '*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate',
-            'Accept-Language': 'zh-CN,zh;q=0.9',
-            'Cache-Control': 'no-cache',
-            'Connection': 'keep-alive',
-            'Host': 'www.mayiwenku.com',
-            'Pragma': 'no-cache',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+            "Accept": "*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "Host": "www.mayiwenku.com",
+            "Pragma": "no-cache",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
         }
 
     def log(self, *messages):
@@ -53,15 +53,15 @@ class MaYiWenKu(object):
             int(w * (1 - w_radio) / 2 + 1),
             int(h * (1 - h_radio) / 2 + 1),
             w - int(w * (1 - w_radio) / 2 + 1),
-            h - int(h * (1 - h_radio) / 2 + 1)
+            h - int(h * (1 - h_radio) / 2 + 1),
         )
         region = img.crop(box)
 
         img1 = pilImage.new(img.mode, size=(box[2] - box[0], box[3] - box[1]))
         img1.paste(region)
-        img1 = img1.resize(size, pilImage.ANTIALIAS)
-        new_image_path = re.sub("(\d+).png", "\\1_handle.png", image_path)
-        img1.save(new_image_path, quality=95)
+        img1 = img1.resize(size, pilImage.LANCZOS)
+        new_image_path = re.sub("(\d+).gif", "\\1_handle.png", image_path)
+        img1.save(new_image_path, quality=100)
 
         img1.close()
         img.close()
@@ -74,7 +74,7 @@ class MaYiWenKu(object):
         # 获取图片列表并排序
         image_list = []
         for image_name in os.listdir(image_dir):
-            if not image_name.endswith(".png"):
+            if not image_name.endswith(".gif"):
                 continue
             if "handle" in image_name:
                 continue
@@ -84,17 +84,19 @@ class MaYiWenKu(object):
         bookPagesData = []
 
         pdf_page_kwargs = {
-            'leftMargin': 0,
-            'rightMargin': 0,
-            'topMargin': 0,
-            'bottomMargin': 0,
+            "leftMargin": 0,
+            "rightMargin": 0,
+            "topMargin": 0,
+            "bottomMargin": 0,
         }
         save_book_name = "{}.pdf".format(image_dir)
 
         for image_name in image_list:
             image_real_path = os.path.join(image_dir, image_name)
             # 处理图片
-            image_real_path = self.cut_image(image_real_path, (int(a4_width * 0.95), int(a4_height * 0.95)))
+            image_real_path = self.cut_image(
+                image_real_path, (int(a4_width * 0.95), int(a4_height * 0.95))
+            )
             image_width, image_height = self.get_image_size(image_real_path)
             data = Image(image_real_path, image_width, image_height)
 
@@ -112,9 +114,11 @@ class MaYiWenKu(object):
 
     def download_image(self, image_url, document_url):
         headers = self.base_headers.copy()
-        headers.update({
-            'Referer': document_url,
-        })
+        headers.update(
+            {
+                "Referer": document_url,
+            }
+        )
         resp = requests.get(image_url, headers=headers)
         return resp.content
 
@@ -123,7 +127,10 @@ class MaYiWenKu(object):
         headers = self.base_headers
         resp = requests.get(document_url, headers=headers)
         #
-        image_base = re.search("input\s*type=\"hidden\"\s*id=\"dp\"\s*value=\"([^\"]+)", resp.text).group(1)
+        # image_base = re.search("input\s*type=\"hidden\"\s*id=\"dp\"\s*value=\"([^\"]+)", resp.text).group(1)
+        image_base = re.search(
+            'input\s*type="hidden"\s*id="dp"\s*value="([^"]+)"', resp.text
+        ).group(1)
         max_page = re.search("var fCount = (\d+);", resp.text).group(1)
         title = re.search("title>(.*?)_蚂蚁文库", resp.text).group(1)
         info = {
@@ -136,24 +143,23 @@ class MaYiWenKu(object):
     def get_document(self, url, max_page=None):
         # 获取文档基本信息
         document_info = self.get_document_info(url)
-        title = document_info['title']
-        image_base_url = document_info['image_base']
-        max_page = max_page or int(document_info['max_page'])
+        title = document_info["title"]
+        image_base_url = document_info["image_base"]
+        max_page = max_page or int(document_info["max_page"])
         #
-        self.log("""文档名称: {}\n总页码: {}\n开始下载...""".format(
-            title, max_page
-        ))
+        self.log("""文档名称: {}\n总页码: {}\n开始下载...""".format(title, max_page))
         #
         image_dir = os.path.join(self.data_dir, "蚂蚁文库_%s" % title)
         os.makedirs(image_dir, exist_ok=True)
 
         for page_index in range(1, max_page + 1):
-            image_path = os.path.join(image_dir, "{}.png".format(page_index))
+            image_path = os.path.join(image_dir, "{}.gif".format(page_index))
             if not os.path.exists(image_path):
-                image_content = self.download_image("{}{}.gif".format(image_base_url, page_index), url)
+                image_part_url = "{}{}.gif".format(image_base_url, page_index)
+                image_content = self.download_image(image_part_url, url)
                 with open(image_path, "wb") as f:
                     f.write(image_content)
-                self.log("\t第{}页下载成功".format(page_index))
+                self.log("\t第{}页下载成功: {}".format(page_index, image_part_url))
                 time.sleep(1)
             else:
                 self.log("\t第{}页已存在".format(page_index))
@@ -162,9 +168,7 @@ class MaYiWenKu(object):
 
 def get_cmd_args():
     parser = argparse.ArgumentParser(
-        prog=u"Program Name (default: sys.argv[0])",
-        description="蚂蚁文库下载器",
-        add_help=True
+        prog="Program Name (default: sys.argv[0])", description="蚂蚁文库下载器", add_help=True
     )
     parser.add_argument("--url", action="store", help="待下载文档url")
     parser.add_argument("--max_page", action="store", help="待下载文档最大页码 默认全部下载")
@@ -189,7 +193,7 @@ def main():
     return
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
     # 测试
     # wenku = MaYiWenKu()
